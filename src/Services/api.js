5d88e03051bf8a007613a7afdfd4679fca95dcd7
marianovungel <vungem88@gmail.com>
2024-04-28 19:51:47 -0300

    trocar url

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/'
-    // baseURL: 'https://tem-w1vm.onrender.com',
+    // baseURL: 'http://localhost:8000/'
+    baseURL: 'https://educaback.onrender.com/',
 });
 
 export default api;
